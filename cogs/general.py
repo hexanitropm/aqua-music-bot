5f87faea6cf61b2f22890936c1d44cbd786c8dee
assa <markwvvxxx1@proton.me>
2026-09-18 12:32:26 +0300

    Connect and disconnect from voice channel

--- cogs/general.py
+++ cogs/general.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 from discord.ext import commands
 
@@ -37,22 +38,22 @@ class General(commands.Cog):
 
     @discord.app_commands.command(name="join")
     async def join(self, interaction: discord.Interaction):
-
         if interaction.user.voice is None:
             await interaction.response.send_message(
-                "❌you are not in the voice channel"
+                "❌ you are not in the voice channel"
             )
             return
 
         if interaction.guild.voice_client is not None:
             await interaction.response.send_message(
-                "❌bot is already on the voice channel"
+                "❌ bot is already on the voice channel"
             )
             return
-        
+
         voice_channel = interaction.user.voice.channel
 
         await voice_channel.connect()
+
         await interaction.response.send_message(
             f"🎧Aqua connected to voice channel: {voice_channel.name}"
         )
@@ -62,11 +63,14 @@ class General(commands.Cog):
 
         if interaction.guild.voice_client is None:
             await interaction.response.send_message(
-                "❌bot is not in the voice channel"
+                "❌ bot is not in the voice channel"
             )
             return
 
         await interaction.guild.voice_client.disconnect()
+        await interaction.response.send_message(
+            "👋 Aqua left the voice channel!"
+        )
         
 async def setup(bot):
     await bot.add_cog(General(bot))
